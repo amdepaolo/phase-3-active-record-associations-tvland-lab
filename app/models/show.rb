2af126d99ac6ae33e75fbc97191b5761ba7c4511
Adam DePaolo <amdepaolo@gmail.com>
2022-10-17 16:43:26 -0400

    commit tree 6323401e515c7ca1d65fb9336c0a355de197db40

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        cast = []
+        self.actors.each do |actor|
+            cast << actor.full_name
+        end
+        cast
+    end
   
 end
